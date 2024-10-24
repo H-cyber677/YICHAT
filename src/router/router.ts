9cede18d604d5acb75458adc513356d960458394
H-cyber677 <2350594699@qq.com>
2024-10-24 23:47:40 +0800

    v

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,15 +1,14 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 import Chat from '@/components/Chat.vue';
 import Login from '@/Pages/Login/Login.vue';
 const routes = [
   { path: '/', component: Login },
-
   { path: '/about', component: Chat }
 ]
 
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
