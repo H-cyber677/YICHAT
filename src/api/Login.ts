2a2c89cd92c709517726fed5397aae244cb47699
疯不皮 <2921744480@qq.com>
2024-10-24 17:29:57 +0800

    feat: 封装登录接口

--- src/api/Login.ts
+++ src/api/Login.ts
@@ -1,5 +1,12 @@
-export default class Login {
-  static login() {
+import axiosRequest from "@/utils/axiosRequest"
 
+interface LoginBody {
+  username: string
+  password: string
+}
+
+export default class Login {
+  static login(body: LoginBody) {
+    return axiosRequest.post('/login', body)
   }
 }
