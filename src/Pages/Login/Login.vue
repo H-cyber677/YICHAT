a59a02488adab6759c1a864d2441f36c2bab531f
H-cyber677 <2350594699@qq.com>
2024-10-24 17:21:18 +0800

    YICHAT

--- src/Pages/Login/Login.vue
+++ src/Pages/Login/Login.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="Login">
-    <div class="loginimg" :style="{backgroundImage:`url(${backgroundimg})`}"></div>
-      <p class="title">Hello,Welcome Yicha!</p>
-             <form action="/submit" method="post">
-              <div class="form-group">
-              <label for="username"></label>
-              <input type="text" id="username" name="username" placeholder="用户名ID" required /><br>
-              </div>
-              <div class="form-group">
-              <label for="password"></label>
-              <input type="pwd" name="password" id="password" placeholder="密码" required />
-              </div>
-              <div class="reminder">
-              <div class="checkbox">
-              <input type="checkbox" name="remember" id="remember"  class="remember" required/>
-              <label for="remember-me">记住密码</label>
-              </div>
-              <div class="text">
-                <p>忘记密码,<a href="">立即注册</a></p>
-              </div>
-              </div>
-              
-              <button type="submit" id="login">登录</button>
-             </form>
+    <div class="loginimg" :style="{ backgroundImage: `url(${backgroundimg})` }"></div>
+    <p class="title">Hello,Welcome Yicha!</p>
+    <div class="box">
 
-   
+      <form action="/submit" method="post">
+        <div class="form-group">
+          <label for="username"></label>
+          <input type="text" id="username" name="username" placeholder="用户名ID" required /><br>
+        </div>
+        <div class="form-group">
+          <label for="password"></label>
+          <input type="pwd" name="password" id="password" placeholder="密码" required />
+        </div>
+
+        <div class="reminder">
+          <div class="checkbox">
+            <input type="checkbox" name="remember" id="remember" class="remember" required />
+            <label for="remember-me">记住密码</label>
+          </div>
+          <div class="text">
+            <p>忘记密码,<a href="">立即注册</a></p>
+          </div>
+        </div>
+
+        <button type="submit" class="login">登录</button>
+      </form>
+    </div>
+
   </div>
 
 </template>
@@ -61,7 +64,17 @@ import backgroundimg from "@/assets/image/backgroundimg.jpg";
   background-size: cover;
   
 } */
-.loginimg{
+.Login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.box {
+  display: flex;
+}
+
+.loginimg {
   width: 100%;
   height: 100%;
   position: absolute;
@@ -74,57 +87,51 @@ import backgroundimg from "@/assets/image/backgroundimg.jpg";
   background-size: cover;
   filter: blur(8px);
 }
-.title{
+
+.title {
   color: #fff;
   font-size: 50px;
-  display: flex;
-  justify-content: center;
+
 }
-input{
+
+input {
   width: 300px;
   height: 30px;
 }
-input:hover{
+
+input:hover {
   background-color: #d6d1d1;
 }
-.form-group{
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+
+.form-group {
   margin-bottom: 50px;
   transform: translateY(70px);
   font-size: 20px;
 }
-#login{
+
+.login {
   width: 300px;
   height: 30px;
   font-size: 20px;
   color: #fff;
   background-color: rgb(11, 119, 220);
   border: none;
-  transition: transform 1s;
-  transform: translate(540px,30px);
+
 }
-#login:hover{
+
+.login:hover {
   background-color: rgb(45, 73, 188);
 }
 
-.remember{
+.remember {
   width: 20px;
   height: 20px;
 }
-.reminder{
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin-top: 80px;
-  gap: 30px;
-}
-.checkbox{
+
+.reminder {
   display: flex;
   align-items: center;
-}
-.text{
-  display: flex;
+  margin-top: 80px;
+  gap: 100px;
 }
 </style>
